2025a3ecfcdc2ecdd9e827e1dbd342b937c0b11f
Tobin Tojo <69814148+TobinTojo@users.noreply.github.com>
2024-07-02 15:30:00 -0400

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Hero from "./components/Hero"
+
+export default function App() {  
+    return (
+        <div>
+            <Hero />
+        </div>
+    )
+}
