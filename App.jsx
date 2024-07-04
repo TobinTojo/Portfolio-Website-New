363381b3a06c1a28de96cb785991c9721a3a8fdb
Tobin Tojo <69814148+TobinTojo@users.noreply.github.com>
2024-07-04 15:38:50 -0400

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Hero from "./components/Hero"
+import Hero from "./components/Hero";
+import About from "./components/About";
+import Skills from "./components/Skills";
+import Projects from "./components/Projects";
 
 export default function App() {  
     return (
         <div>
             <Hero />
+            <About />
+            <Skills />
+            <Projects />
         </div>
     )
 }
