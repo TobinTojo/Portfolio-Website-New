a769fec45389feee69026d736d7baf1aeabd69cb
Tobin Tojo <69814148+TobinTojo@users.noreply.github.com>
2024-07-08 16:02:54 -0400

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -5,6 +5,7 @@ import About from "./components/About";
 import Skills from "./components/Skills";
 import Projects from "./components/Projects";
 import Experience from "./components/Experience";
+import Contact from "./components/Contact";
 
 export default function App() {  
     return (
@@ -14,6 +15,7 @@ export default function App() {
             <Skills />
             <Projects />
             <Experience />
+            <Contact />
         </div>
     )
 }
